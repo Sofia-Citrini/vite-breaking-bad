1f2409ecdb124e727367b8e31d600810fceff3bd
Sofia-Citrini <sofiacitrini@gmail.com>
2022-12-06 17:14:23 +0100

    add TheHeader component (title)

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
+  <TheHeader></TheHeader>
+
+  
   
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue'
+
+export default {
+  components: {TheHeader},
+  data(){
+    return{
+      
+    }
+  }
+}
 
 </script>
 
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="p-3 d-flex ">
+        <div class="img-container">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Breaking_Bad_logo.svg/2560px-Breaking_Bad_logo.svg.png" 
+            class="img-fluid" alt="">
+        </div>
+
+        <h1 class="px-3 text-color">Breaking Bad Api</h1>
+    </header>
+</template>
+
+<script></script>
+
+<style scoped lang="scss">
+@use '../styles/general.scss';
+ .img-container {
+    width: 5rem;
+ }
+</style>
